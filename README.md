383f1e79f4b5fd4f47fb77413f01056bbb9b4b16
v <vsunbe@gmail.com>
2024-10-01 16:35:16 +0800

    Pretty-print the board to the terminal

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Life
 
 [x] Build a data structure to store the board state
-[ ] “Pretty-print” the board to the terminal
+[x] “Pretty-print” the board to the terminal
 [ ] Given a starting board state, calculate the next one
 [ ] Run the game forever
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn dead_state(width: usize, height: usize) -> Vec<Vec<usize>> {
     vec![vec![0; width]; height]
 }
 
-fn radom_state(width: usize, height: usize) -> Vec<Vec<usize>> {
+fn random_state(width: usize, height: usize) -> Vec<Vec<usize>> {
     let mut state = dead_state(width, height);
 
     let mut rng = rand::thread_rng();
@@ -19,8 +19,47 @@ fn radom_state(width: usize, height: usize) -> Vec<Vec<usize>> {
     state
 }
 
+fn render(state: Vec<Vec<usize>>) {
+    let DEAD = "#";
+    let ALIVE = "@";
+
+    for (i, row) in state.iter().enumerate() {
+        if i == 0 {
+            for _ in 0..row.len() + 2 {
+                print!("-");
+            }
+            print!("\r\n");
+        }
+
+        for (j, n) in row.iter().enumerate() {
+            if j == 0 {
+                print!("|");
+            }
+
+            if *n == 1 {
+                print!("{}", ALIVE);
+            } else if *n == 0 {
+                print!("{}", DEAD);
+            }
+
+            if j == row.len() - 1 {
+                print!("|");
+            }
+        }
+        print!("\r\n");
+
+        if i == state.len() - 1 {
+            for _ in 0..row.len() + 2 {
+                print!("-");
+            }
+            print!("\r\n");
+        }
+    }
+}
+
 fn main() {
-    let width = 5;
-    let height = 6;
-    println!("{:?}", radom_state(width, height));
+    let dead_state = dead_state(40, 30);
+    render(dead_state);
+    let random_state = random_state(20, 30);
+    render(random_state);
 }
